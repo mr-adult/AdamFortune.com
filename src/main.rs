0a05620049305f6961d1f8d1eb512af028ef765f
Adam Fortune <adamfortune96@protonmail.com>
2023-10-22 14:23:09 -0500

    Remove URL safe name handling

--- src/main.rs
+++ src/main.rs
@@ -355,16 +355,6 @@ fn generate_blog_card(index: usize, blog_post: &BlogPost) -> String {
 
 fn get_url_safe_name(name: &str) -> String {
     return name.to_string();
-    return name.chars()
-        .filter(|char| {
-            match char {
-                '0'..='9'
-                | 'a'..='z'
-                | 'A'..='Z'
-                | '_' => true,
-                _ => false,
-            }
-        }).collect()
 }
 
 fn parse_md_to_html(md: &str) -> String {
